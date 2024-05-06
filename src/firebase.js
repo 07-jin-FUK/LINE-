a8db86e79412468ce215c72516b8b14463163f2d
07-jin-FUK <dedededeen@icloud.com>
2024-05-06 23:28:49 +0900

    LINE first

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyCbyVTNY-4szlMdIYgd1OkIalRxR4rrlIs",
+    authDomain: "gamejin-684bd.firebaseapp.com",
+    databaseURL: "https://gamejin-684bd-default-rtdb.firebaseio.com",
+    projectId: "gamejin-684bd",
+    storageBucket: "gamejin-684bd.appspot.com",
+    messagingSenderId: "1053657359309",
+    appId: "1:1053657359309:web:90a276c1a888d77e799a84"
+});
+
+const db = firebaseApp.firestore();
+
+const auth = firebase.auth();
+
+export { db, auth }
+
